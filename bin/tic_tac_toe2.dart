bcac8916db9a8d2ae60c8d337a23cb33c208dd97
KaianS <103215707+KaianS@users.noreply.github.com>
2022-04-12 16:22:44 -0300

    docs: add draw comments

--- bin/tic_tac_toe2.dart
+++ bin/tic_tac_toe2.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 
-enum Status {none, circle, cross}
+enum Status { none, circle, cross }
 
-int playerTime = 1;
+int playerTime = 0;
 bool isWon = false;
 Status verifyTurn = Status.cross;
-
+bool isGameFinished = false;
 
 List<Status> board = [
   Status.none,
@@ -19,112 +19,137 @@ List<Status> board = [
   Status.none,
 ];
 
-void main(List<String> arguments){
+// All the winning sequences
+List<List> winningMatches = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+void main(List<String> arguments) {
+  while (!isGameFinished) {
+    game();
+  }
+}
+
+// Game flux
+void game() {
   showBoard();
-  verifyIfPlayerWon();
-  changeTurn();
   getInput();
+  verifyIfPlayerWon();
   clearScreen();
 }
 
-String convertStatusToString(int position){
-  switch(board[position]){
-    case Status.none:
-      return '$position';
-    case Status.circle:
-      return 'O';
-    case Status.cross:
-      return 'X';
-    default:
-      return 'Por favor, digite um valor válido:';  
-  }
-}
-
-void changeTurn(){
-  playerTime++;
-
-  if(playerTime.isEven == true){
-    print('Jogador X, escolha um espaço.');
-  } else if(playerTime.isEven == false) {
-    print('Jogador O, escolha um espaço.');
-  }
-
-  verifyIfPlayerWon();
-  getInput();
-  showBoard();
-  changeTurn();
-}
-
-String getInput(){
+// Get user input
+void getInput() {
+  print('Jogador ${convertStatusToString(verifyTurn)}, faça sua jogada');
   var userInput = stdin.readLineSync();
 
   var inputNumber = int.tryParse(userInput.toString());
 
-  verifyTurn = verifyTurn == Status.cross ? Status.circle : Status.cross;
-
   if (inputNumber != null) {
-    var updatedTile = verifyTurn == Status.circle ? Status.cross : Status.circle;
+    var updatedTile = verifyTurn;
     board[inputNumber] = updatedTile;
-  } 
-
-  return userInput;
-}
-
-void checkInputDisponibility(){
-  
-}
-
-void updateBoardPlayer(){
-
-}
-
-void verifyIfPlayerWon(){
-
-  //all possibilities to X win
-  if((board[0] == Status.cross) & (board[1] == Status.cross) & (board[2] == Status.cross)){
-    print('Jogador [X] ganhou o jogo!');
-  } else if((board[3] == Status.cross) & (board[4] == Status.cross) & (board[5] == Status.cross)){
-    print('Jogador [X] ganhou o jogo!');
-  } else if((board[3] == Status.cross) & (board[4] == Status.cross) & (board[5] == Status.cross)){
-    print('Jogador [X] ganhou o jogo!');
-  } else if((board[6] == Status.cross) & (board[7] == Status.cross) & (board[8] == Status.cross)){
-    print('Jogador [X] ganhou o jogo!');
-  } else if((board[0] == Status.cross) & (board[4] == Status.cross) & (board[8] == Status.cross)){
-    print('Jogador [X] ganhou o jogo!');
-  } else if((board[2] == Status.cross) & (board[4] == Status.cross) & (board[6] == Status.cross)){
-    print('Jogador [X] ganhou o jogo!');
-  } else if((board[0] == Status.cross) & (board[3] == Status.cross) & (board[6] == Status.cross)){
-    print('Jogador [X] ganhou o jogo!');
-  } else if((board[1] == Status.cross) & (board[4] == Status.cross) & (board[7] == Status.cross)){
-    print('Jogador [X] ganhou o jogo!');
-  } else if((board[2] == Status.cross) & (board[5] == Status.cross) & (board[8] == Status.cross)){
-    print('Jogador [X] ganhou o jogo!');
   }
 
-  //all possibilities to O win
-  if((board[0] == Status.circle) & (board[1] == Status.circle) & (board[2] == Status.circle)){
-    print('Jogador [O] ganhou o jogo!');
-  } else if((board[3] == Status.circle) & (board[4] == Status.circle) & (board[5] == Status.circle)){
-    print('Jogador [O] ganhou o jogo!');
-  } else if((board[3] == Status.circle) & (board[4] == Status.circle) & (board[5] == Status.circle)){
-    print('Jogador [O] ganhou o jogo!');
-  } else if((board[6] == Status.circle) & (board[7] == Status.circle) & (board[8] == Status.circle)){
-    print('Jogador [O] ganhou o jogo!');
-  } else if((board[0] == Status.circle) & (board[4] == Status.circle) & (board[8] == Status.circle)){
-    print('Jogador [O] ganhou o jogo!');
-  } else if((board[2] == Status.circle) & (board[4] == Status.circle) & (board[6] == Status.circle)){
-    print('Jogador [O] ganhou o jogo!');
-  } else if((board[0] == Status.circle) & (board[3] == Status.circle) & (board[6] == Status.circle)){
-    print('Jogador [O] ganhou o jogo!');
-  } else if((board[1] == Status.circle) & (board[4] == Status.circle) & (board[7] == Status.circle)){
-    print('Jogador [O] ganhou o jogo!');
-  } else if((board[2] == Status.circle) & (board[5] == Status.circle) & (board[8] == Status.circle)){
-    print('Jogador [O] ganhou o jogo!');
-  } 
-
-
+  // Switch turns
+  changeTurn();
 }
 
+void checkInputDisponibility() {}
+
+// Check if there's a winner
+void verifyIfPlayerWon() {
+  // Check if it's a draw
+  if (playerTime == 9) {
+    finishGame(Status.none);
+  }
+
+  for (var i = 0; i < 8; i++) {
+    // Contador
+    var counter = 0;
+
+    // Iterating each array
+    // Ex: [0, 1, 2]
+    // Add +1 for X ou -1 for circle in the counter
+    winningMatches[i].forEach((element) {
+      if (board[element] == Status.cross) {
+        counter++;
+      } else if (board[element] == Status.circle) {
+        counter--;
+      }
+    });
+
+    if (counter == 3) {
+      finishGame(Status.cross);
+    } else if (counter == -3) {
+      finishGame(Status.circle);
+    }
+  }
+}
+
+// Switch players
+void changeTurn() {
+  playerTime++;
+
+  if (verifyTurn == Status.cross) {
+    verifyTurn = Status.circle;
+  } else {
+    verifyTurn = Status.cross;
+  }
+}
+
+// Finish game
+// Ask if the user wants to keep playing
+void finishGame(Status winner) {
+  showBoard();
+
+  if (winner == Status.none) {
+    print('Empate! Ninguém ganhou o jogo!');
+  } else {
+    print('Parabéns! ${winner == Status.cross ? "X" : "O"} ganhou o jogo!');
+  }
+
+  isGameFinished = true;
+  print('Quer jogar novamente? S / N');
+  var availableAnswers = ['S', 's', 'N', 'n'];
+  var userChoice = stdin.readLineSync();
+  if (availableAnswers.contains(userChoice)) {
+    if (userChoice == 'S' || userChoice == 's') {
+      resetGame();
+    }
+    if (userChoice == 'N' || userChoice == 'n') {
+      clearScreen();
+    }
+  }
+}
+
+// Reset game to initial values
+void resetGame() {
+  playerTime = 1;
+  isWon = false;
+  verifyTurn = Status.cross;
+  isGameFinished = false;
+
+  board = [
+    Status.none,
+    Status.none,
+    Status.none,
+    Status.none,
+    Status.none,
+    Status.none,
+    Status.none,
+    Status.none,
+    Status.none,
+  ];
+}
+
+// Clear terminal
 void clearScreen() {
   if (Platform.isWindows) {
     print(Process.runSync('cls', [], runInShell: true).stdout);
@@ -133,13 +158,40 @@ void clearScreen() {
   }
 }
 
+String createBoardCharacters(int position) {
+  switch (board[position]) {
+    case Status.none:
+      return '$position';
+    case Status.circle:
+      return 'O';
+    case Status.cross:
+      return 'X';
+    default:
+      return 'Por favor, digite um valor válido:';
+  }
+}
+
+String convertStatusToString(Status status) {
+  switch (status) {
+    case Status.none:
+      return 'none';
+    case Status.cross:
+      return 'X';
+    case Status.circle:
+      return 'O';
+  }
+
+
+  return null;
+}
+
 void showBoard() {
   print(
-      '  ${convertStatusToString(0)}  |  ${convertStatusToString(1)}  |  ${convertStatusToString(2)}  ');
+      '  ${createBoardCharacters(0)}  |  ${createBoardCharacters(1)}  |  ${createBoardCharacters(2)}  ');
   print('-----|-----|-----');
   print(
-      '  ${convertStatusToString(3)}  |  ${convertStatusToString(4)}  |  ${convertStatusToString(5)}  ');
+      '  ${createBoardCharacters(3)}  |  ${createBoardCharacters(4)}  |  ${createBoardCharacters(5)}  ');
   print('-----|-----|-----');
   print(
-      '  ${convertStatusToString(6)}  |  ${convertStatusToString(7)}  |  ${convertStatusToString(8)}  ');
+      '  ${createBoardCharacters(6)}  |  ${createBoardCharacters(7)}  |  ${createBoardCharacters(8)}  ');
 }
